766e181b2fd1311400b3b5a298fb63cec40c84ce
ndrplm <iamandreapalma@gmail.com>
2022-04-09 15:40:36 +0200

    Add player form wip

--- src/pages/games/new/steps/Players.jsx
+++ src/pages/games/new/steps/Players.jsx
@@ -1,18 +1,33 @@
 import { Input, InputLabel, Stack, Typography } from "@mui/material";
+import { Field, FieldArray, Form, Formik } from "formik";
 import { useNavigate } from "react-router-dom";
 import { NewGameNavigationButtons } from "../private/NewGameNavigationButtons";
 
 export const Players = () => {
   const navigate = useNavigate()
+  const initialValues = {
+    players: []
+  }
   return (
     <Stack spacing={8}>
       <Typography variant="h6" component="h1">
         Ajouter des joueurs
       </Typography>
-      <Stack>
-        <InputLabel>Joueur</InputLabel>
-        <Input />
-      </Stack>
+      <Formik initialValues={initialValues}>
+        {({values}) => (
+          <Form>
+            {console.log(values)}
+            <Stack>
+              <InputLabel>Joueur</InputLabel>
+              <FieldArray name="players">
+                {() => (
+                  <Field/>
+                )}
+              </FieldArray>
+            </Stack>
+          </Form>
+        )}
+      </Formik>
       <NewGameNavigationButtons hasPrevious hasNext previous={() => navigate("/")} />
     </Stack>
   );
