7256844fa74dcae23ffb4ef6a814d28a9cb44d19
ndrplm <iamandreapalma@gmail.com>
2022-08-23 20:16:23 +0200

    👑 Create types file

---
+++ src/types.tsx
@@ -0,0 +1,42 @@
+export interface Player {
+  name: string
+  id: number
+}
+
+export interface Game {
+  players: Player[]
+  hands?: Hand[]
+}
+
+export interface Hand {
+  id: number
+  taker: Taker
+  defense: Defense
+}
+
+export interface Defense {
+  players: Player[]
+  bonuses: Bonus[]
+  score: number
+}
+
+export interface Taker {
+  player: Player
+  partner?: Player
+  bet: Bet
+  oudlersCount: number
+  pointsCount: number
+  bonuses: Bonus[]
+  score: number
+}
+
+export interface Bonus {
+  name: 'chelem' | 'poignée' | 'petit au bout'
+  points: number
+  isMultiplied: boolean
+}
+
+export interface Bet {
+  name: 'petite' | 'garde' | 'garde sans' | 'garde contre'
+  multiplier: number
+}
