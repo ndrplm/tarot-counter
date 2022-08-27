7b753bf3d2f00c97e62c27771defe4b2f972ee69
ndrplm <iamandreapalma@gmail.com>
2022-08-27 12:53:06 +0200

    Write tests for sum function

---
+++ src/pages/hands/_handId/helpers/helpers.test.ts
@@ -0,0 +1,35 @@
+import { objSum, SumParam } from './helpers'
+
+const positiveValues: SumParam = {
+  key1: 1,
+  key2: 3,
+  key3: 5,
+}
+
+const negativeValues: SumParam = {
+  key4: -10,
+  key5: 3,
+  key6: -5,
+}
+
+const floatValues: SumParam = {
+  key7: 1.12,
+  key8: 4.65,
+  key9: 8.77,
+}
+
+describe('Unit test - objSum', () => {
+  it("returns the sum of an object's values - positive values", () => {
+    expect(objSum(positiveValues)).toBe(9)
+  })
+  it("returns the sum of an object's values - negative values", () => {
+    expect(objSum(negativeValues)).toBe(-12)
+  })
+  it("returns the sum of an object's values - float values", () => {
+    expect(objSum(floatValues)).toBe(14.54)
+  })
+
+  it("returns the sum of an object's values - all of the above", () => {
+    expect(objSum({ ...positiveValues, ...negativeValues, ...floatValues })).toBe(11.54)
+  })
+})
