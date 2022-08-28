afb13ff5eebaf468afc9416dd1ec69a3707b72dc
ndrplm <iamandreapalma@gmail.com>
2022-08-28 22:42:32 +0200

    More robust tests on points count

--- src/pages/hands/_handId/helpers/helpers.test.ts
+++ src/pages/hands/_handId/helpers/helpers.test.ts
@@ -1,5 +1,5 @@
-import { Hand, Player } from '../../../../types'
 import { calculateFinalScore, objSum, SumParam } from './helpers'
+import { caseOne, caseThree, caseTwo, caseFour, caseFive } from './mocks'
 
 const positiveValues: SumParam = {
   key1: 1,
@@ -35,36 +35,25 @@ describe('Unit test - objSum', () => {
   })
 })
 
-// Le preneur tente une Garde, présente une Poignée de 10 Atouts. Il mène le Petit
-// au Bout et réalise 49 points en détenant deux Bouts. Il passe donc de 49 - 41 = 8
-// o 25 (contrat) + 8 = 33 multiplié par 2 (Garde) = 66
-// o Poignée = 20
-// o Petit au Bout 10 X 2 = 20
-// Le nombre de points est égal à 66 + 20 + 20 = + 106
-// Chaque Défenseur marque -106 et le preneur marque 106 X 3 = +318.
-
-describe('Unit test - calculareFinalScore', () => {
-  const players: Player[] = [
-    { id: '1', name: 'Andréa' },
-    { id: '2', name: 'Fortunato' },
-    { id: '3', name: 'Béatrice' },
-    { id: '4', name: 'Alyx' },
-  ]
-  const hand: Hand = {
-    taker: {
-      playerId: '1',
-      betName: 'garde',
-      oudlersCount: 2,
-      pointsCount: 49,
-    },
-    defendeurs: ['2', '3', '4'],
-    bonuses: [
-      { name: 'poignee', playerID: '1', type: 'simple' },
-      { name: 'petitAuBout', playerID: '1' },
-      { name: 'chelem', playerID: '' },
-    ],
-  }
-  it('works', () => {
+describe('Unit test - calculateFinalScore', () => {
+  it('returns the right value for caseOne', () => {
+    const { players, hand } = caseOne
     expect(calculateFinalScore(hand, players)).toEqual({ taker: 318, defendeurs: -106 })
+  })
+  it('returns the right value for caseTwo', () => {
+    const { players, hand } = caseTwo
+    expect(calculateFinalScore(hand, players)).toEqual({ taker: 228, defendeurs: -76 })
+  })
+  it('returns the right value for casetThree', () => {
+    const { players, hand } = caseThree
+    expect(calculateFinalScore(hand, players)).toEqual({ taker: -126, defendeurs: 42 })
+  })
+  it('returns the right value for caseFour', () => {
+    const { players, hand } = caseFour
+    expect(calculateFinalScore(hand, players)).toEqual({ taker: 1746, defendeurs: -582 })
+  })
+  it('returns the right value for caseFive', () => {
+    const { players, hand } = caseFive
+    expect(calculateFinalScore(hand, players)).toEqual({ taker: 80, defendeurs: -40, partner: 40 })
   })
 })
